701178abeef4e1f7c0e7cd490a8872b6c98175ae
flux423 <admin@423interactive.com>
2015-04-27 10:16:47 -0400

    fix github

--- github.php
+++ github.php
@@ -1,14 +1,1 @@
-<?php `
-while read oldrev newrev ref
-do
-    if [[ $ref =~ .*/master$ ]];
-    then
-        echo "Master ref received.  Deploying master branch to LIVE SERVER -- MAKE SURE TO TEST..."
-        git --work-tree=/home/87928/domains/client.423interactive.com/html/github_autopush --git-dir=/home/87928/domains/client.423interactive.com/html/github_autopush/.git checkout -f
-
-    else
-        echo "Ref $ref successfully received.  Doing nothing: only the master branch may be deployed on this server."
-    fi
-done
-
-`;
+<?php `git pull`;
