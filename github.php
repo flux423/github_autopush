559cd32deb02f6dd3c7ef616e5b64d98e1fd419d
Thomas P. Scola Jr. <flux423@users.noreply.github.com>
2015-04-27 14:58:21 -0400

    Update github.php

--- github.php
+++ github.php
@@ -1,13 +1,1 @@
-<?php
-#!/bin/bash
-`while read oldrev newrev ref
-do
-    if [[ $ref =~ .*/dev$ ]];
-    then
-        echo "DEV ref received.  Deploying master branch to DEV SERVER -- MAKE SURE TO TEST..."
-        git --work-tree=/home/87928/domains/client.423interactive.com/html/github_autopush_dev --git-dir=/home/87928/domains/client.423interactive.com/html/github_autopush_dev/.git checkout -f
-
-    else
-        echo "Ref $ref successfully received.  Doing nothing: only the DEV branch may be deployed on this server."
-    fi
-done`
+<?php `git pull`;
